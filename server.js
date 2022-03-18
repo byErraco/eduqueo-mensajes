80550ab702253ce436804c1a02443d5b54435858
Jesus Diaz <erraco.wow@gmail.com>
2022-03-17 22:06:19 -0400

    update 2

--- server.js
+++ server.js
@@ -31,22 +31,22 @@ require("./app/routes/tutorial.routes.js")(app);
 const Bree = require('bree')
 const bree = new Bree({
   jobs : [
-    // runs the job on Start
-    // {
-    //   name : 'metrics',
-    //   interval : '20s' //run the script after 200 seconds from the start
-    // },
-    // {
-    //   name : 'sample', //send msg between 30 and 60 days
-    //   interval : '20s' //run the script after 200 seconds from the start
-    // },
+    // // runs the job on Start
     {
-      name : 'sampletwo',
+      name : 'metrics', //create metrics for the database in airtable
+      interval : '20s' //run the script after 200 seconds from the start
+    },
+    {
+      name : 'sample', //send msg between 30 and 60 days
+      interval : '20s' //run the script after 200 seconds from the start
+    },
+    {
+      name : 'sampletwo', // create sesion info for clients in airtable
       interval : '30s' //run the script after 30 seconds from the start
     }
   ]
 })
-bree.start()
+// bree.start()
 
 
 // set port, listen for requests
